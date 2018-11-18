1810fa862f963a0ee0027842104f09d6f485ec26
vinicomelo <vinicomelo@gmail.com>
2018-11-18 18:23:24 -0500

    cleaning up removed sidenav

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,9 +4,10 @@ import {PostsComponent} from './components/posts/posts.component';
 import {PostDetailComponent} from './components/post-detail/post-detail.component';
 
 const routes: Routes = [
+  {path: '', redirectTo: '/posts', pathMatch: 'full'},
   {path: 'posts', component: PostsComponent},
   {path: 'posts/:id', component: PostDetailComponent},
-  {path: '', redirectTo: '/posts', pathMatch: 'full'}
+  {path: '**', redirectTo: '/posts'}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,14 +6,12 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { PostsComponent } from './components/posts/posts.component';
 import { PostDetailComponent } from './components/post-detail/post-detail.component';
-import { SidenavComponent } from './components/sidenav/sidenav.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PostsComponent,
-    PostDetailComponent,
-    SidenavComponent
+    PostDetailComponent
   ],
   imports: [
     BrowserModule,
