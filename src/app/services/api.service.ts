110ee9d0d0f2dad4061f885935b7c034471157b2
vinicomelo <vinicomelo@gmail.com>
2018-11-18 18:52:15 -0500

    Added api service

---
+++ src/app/services/api.service.ts
@@ -0,0 +1,38 @@
+import {Injectable} from '@angular/core';
+import {Observable, of, throwError} from 'rxjs';
+import {HttpClient, HttpHeaders, HttpErrorResponse} from '@angular/common/http';
+import {catchError, tap, map} from 'rxjs/operators';
+import {Post} from '../models/post';
+
+const httpOptions = {
+  headers: new HttpHeaders({'Content-Type': 'application/json'})
+};
+const apiUrl = 'https://jsonplaceholder.typicode.com/';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  constructor(private http: HttpClient) {
+  }
+
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
+  }
+
+  getPosts(): Observable<Post[]> {
+    return this.http.get<Post[]>(apiUrl)
+      .pipe(
+        tap(res => console.log('fetched posts')),
+        catchError(this.handleError('getPosts', []))
+      );
+  }
+}
