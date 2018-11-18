1cbff001e898945e73c1a88617b07f1c43cf8a45
vinicomelo <vinicomelo@gmail.com>
2018-11-18 16:51:55 -0500

    Added posts and post-detail components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { PostsComponent } from './components/posts/posts.component';
+import { PostDetailComponent } from './components/post-detail/post-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostsComponent,
+    PostDetailComponent
   ],
   imports: [
     BrowserModule,
