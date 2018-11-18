1cffd20614bf344c2d526eda03b54844b244934a
vinicomelo <vinicomelo@gmail.com>
2018-11-18 17:17:22 -0500

    Set up routes for posts and posts/:id

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+import {PostsComponent} from './components/posts/posts.component';
+import {PostDetailComponent} from './components/post-detail/post-detail.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'posts', component: PostsComponent},
+  {path: 'posts/:id', component: PostDetailComponent},
+  {path: '', redirectTo: '/posts', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
