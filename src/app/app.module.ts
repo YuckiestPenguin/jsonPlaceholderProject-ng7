180d203ac9dfbc964f6bffdcddf0130e88108f9c
vinicomelo <vinicomelo@gmail.com>
2018-11-18 19:14:03 -0500

    added progress linear

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,14 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { PostsComponent } from './components/posts/posts.component';
-import { PostDetailComponent } from './components/post-detail/post-detail.component';
-import { MyNavComponent } from './my-nav/my-nav.component';
-import { LayoutModule } from '@angular/cdk/layout';
-import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule } from '@angular/material';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {PostsComponent} from './components/posts/posts.component';
+import {PostDetailComponent} from './components/post-detail/post-detail.component';
+import {MyNavComponent} from './my-nav/my-nav.component';
+import {LayoutModule} from '@angular/cdk/layout';
+import {MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule, MatGridListModule, MatProgressBarModule} from '@angular/material';
 import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
@@ -28,9 +28,12 @@ import {HttpClientModule} from '@angular/common/http';
     MatButtonModule,
     MatSidenavModule,
     MatIconModule,
-    MatListModule
+    MatListModule,
+    MatGridListModule,
+    MatProgressBarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
--- src/app/components/posts/posts.component.ts
+++ src/app/components/posts/posts.component.ts
@@ -9,6 +9,7 @@ import {Post} from '../../models/post';
 })
 export class PostsComponent implements OnInit {
   data: Post[] = [];
+  isLoadingResults = true;
 
   constructor(private api: ApiService) {
   }
@@ -18,8 +19,10 @@ export class PostsComponent implements OnInit {
       .subscribe(res => {
         this.data = res;
         console.log(this.data);
+        this.isLoadingResults = false;
       }, err => {
         console.log(err);
+        this.isLoadingResults = false;
       });
   }
 
